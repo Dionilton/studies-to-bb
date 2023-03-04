317ba7c1e1fa681ea4c82fd227a307432d465e75
Dionilton <dionilton.os@gmail.com>
2023-03-04 12:39:06 -0300

    Nome genérico das classes e funções para o flahs card

--- flashcards/java/Algorritmo.java
+++ flashcards/java/Algorritmo.java
@@ -1,11 +1,11 @@
-public class LinearSearch {
+public class Algorritmo {
     public static void main(String[] args){
         int a[] = {21,22,23,27,29,75,135};
         int chave = 30;
-        System.out.println(linearSearch(a, chave));
+        System.out.println(algorritmo(a, chave));
     }
 
-    public static boolean linearSearch(int vec[], int key){
+    public static boolean algorritmo(int vec[], int key){
         for(int i=0; i< vec.length; i++){
             if(vec[i] == key)
                 return true;
